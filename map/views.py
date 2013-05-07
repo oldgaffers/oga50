41e8ce3c465e3350c88e6012fc7e93c5d3d91081
Julian Cable <julian.cable@yahoo.com>
2013-05-07 20:01:46 +0100

    home

--- map/views.py
+++ map/views.py
@@ -2,6 +2,6 @@
 from django.http import render
 
 def index(request):
-   latest_map = Boat.objects.all().order_by('-pub_date')[:5]
+	latest_map = Boat.objects.all().order_by('-pub_date')[:5]
     context = {'latest_map': latest_map}
     return render(request, 'map/index.html', context)
