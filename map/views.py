7ef4690a8351727e177497d50b1de2b7ec2668c3
Julian Cable <julian.cable@yahoo.com>
2013-05-07 20:12:01 +0100

    home

--- map/views.py
+++ map/views.py
@@ -1,5 +1,7 @@
 # Create your views here.
-from django.http import render
+from django.shortcuts import render
+from map.models import Boat
+
 
 def index(request):
     latest_map = Boat.objects.all()
