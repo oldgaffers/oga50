623c44f150974141a6ba459c6aa6f8113f00a926
Julian Cable <julian.cable@yahoo.com>
2013-05-07 20:00:09 +0100

    home

--- map/views.py
+++ map/views.py
@@ -1,5 +1,7 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.http import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the map index.")
+   latest_map = Boat.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_map': latest_map}
+    return render(request, 'map/index.html', context)
