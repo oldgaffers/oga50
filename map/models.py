aed2c7fda3127540aefcdd3ba008af1199cc6766
Julian Cable <julian.cable@yahoo.com>
2013-05-07 20:58:47 +0100

    home

--- map/models.py
+++ map/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class Boat(models.Model):
     name = models.CharField(max_length=70)
-	lat = models.FloatField()
-	lng = models.FloatField()
+    lat = models.FloatField()
+    lng = models.FloatField()
 
     def __unicode__(self):
         return self.name
