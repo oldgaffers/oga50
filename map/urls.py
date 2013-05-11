391cf8d65dac0c1e679a081a20f24ce0e812cfda
Julian Cable <julian@ronf.local>
2013-05-11 16:58:47 +0100

    add static url pattern

--- map/urls.py
+++ map/urls.py
@@ -5,3 +5,7 @@ from map import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index')
 )
+
+urlpatterns += patterns('',
+ (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+ )
