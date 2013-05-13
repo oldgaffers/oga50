d3091ce1dfaa3845bfed0976a87e05c258e6ae36
Julian Cable <julian@ronf.local>
2013-05-13 04:54:01 +0100

    fetch

--- map/views.py
+++ map/views.py
@@ -4,47 +4,6 @@ from map.models import Boat
 import xml.etree.ElementTree as ET
 import urllib2
 
-class TBoat:
-    def __init__(self, name=None):
-        if name is None:
-            self.name=''
-            self.image=''
-            self.popup=''
-            self.pin=''
-        else:
-            try:
-                b = Boat.objects.get(tpname=name)
-                self.name = b.name
-                self.image = b.image
-                self.pin = b.pin
-                self.popup='<b>'+b.name+'</b>'
-                if b.image != '':
-                    self.popup = self.popup + "</br><img width='100' height='100' src='/map/static/map/"+b.image+"'/>"
-                if b.blog!= '':
-                    self.popup = self.popup + "</br><a href='"+b.blog+"'>View log</a>"
-            except Boat.DoesNotExist:
-                self.name = name
-                self.image=''
-                self.popup=''
-                self.pin='manualIcon'
-        self.lat = 0.0
-        self.lng = 0.0
-
-def trackaphone():
-    r = urllib2.urlopen("http://trackaphone.co.uk/callback/publish?id=1366120222963T569D3PYVN9B")
-    xml = r.read()
-    root = ET.fromstring(xml)
-    boats = []
-    for n in root[1]:
-        device = n.attrib
-        loc = n[0].attrib
-        name = device['name']
-        b = TBoat(name)
-        b.lat = loc['lat']
-        b.lng = loc['lng']
-        boats.append(b)
-    return boats
-
 def ais(boat):
     r = urllib2.urlopen("http://marinetraffic.com/ais/shipdetails.aspx?MMSI=" + boat.mmsi)
     html = r.read()
@@ -57,9 +16,15 @@ def ais(boat):
     return boat
 
 def index(request):
-    local_db = Boat.objects.exclude(mmsi='')
+    local_db = Boat.objects.all()
     boats = []
     for b in local_db:
-        boats.append(ais(b))
-    context = {'ais': boats, 'trackaphone': trackaphone()}
+        b.popup=''
+        if b.image != '':
+            b.popup = "<b>'+b.name+'</b></br><img width='100' height='100' src='/map/static/map/"+b.image+"'/>"
+        if b.blog!= '':
+            b.popup = b.popup + "</br><a href='"+b.blog+"'>View log</a>"
+        boats.append(b)
+    #context = {'ais': boats, 'trackaphone': trackaphone()}
+    context = {'boats': boats}
     return render(request, 'map/index.html', context)
