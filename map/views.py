75890671170e438e6f48522f87eb543177ac2184
Julian Cable <julian.cable@yahoo.com>
2013-05-17 23:33:14 +0100

    add last fix to popup, make all boats have at least name as popup

--- map/views.py
+++ map/views.py
@@ -1,31 +1,19 @@
 # Create your views here.
 from django.shortcuts import render
 from map.models import Boat
-import xml.etree.ElementTree as ET
-import urllib2
-
-def ais(boat):
-    r = urllib2.urlopen("http://marinetraffic.com/ais/shipdetails.aspx?MMSI=" + boat.mmsi)
-    html = r.read()
-    p1 = html.find("&deg;")
-    p2 = html.rfind(">", 0, p1) + 1
-    p3 = html.find("&deg;", p1+1)
-    p4 = html.rfind("/", p1, p3) + 1
-    boat.lat = float(html[p2:p1])
-    boat.lng = float(html[p4:p3])
-    return boat
 
 def index(request):
     local_db = Boat.objects.all()
     boats = []
     for b in local_db:
-        b.popup=''
+        b.popup = "<b>"+b.name+"</b>"
         if b.image.startswith('http:'):
-            b.popup = "<b>"+b.name+"</b></br><img width='100' height='100' src='"+b.image+"'/>"
+            b.popup = b.popup + "</br><img width='100' height='100' src='"+b.image+"'/>"
         elif b.image != '':
             b.popup = "<b>"+b.name+"</b></br><img width='100' height='100' src='/map/static/map/"+b.image+"'/>"
         if b.blog!= '':
             b.popup = b.popup + "</br><a href='"+b.blog+"'>View log</a>"
+        b.popup = b.popup + "</br>last fix: "+b.last_fix
         boats.append(b)
     context = {'boats': boats}
     return render(request, 'map/index.html', context)
